189460486f31484a0af8ed7bc01b75b3d4951b4a
Shrey Dixit <shreyakshaj@gmail.com>
2024-03-20 09:51:21 +0100

    updated version calc

--- msapy/__init__.py
+++ msapy/__init__.py
@@ -1,12 +1,4 @@
 from msapy import msa, utils, checks
-import toml
-import os
 
-package_dir = os.path.abspath(os.path.dirname(__file__))
-pyproject_path = os.path.join(package_dir, "..", "pyproject.toml")
-
-try:
-    __version__ = toml.load(pyproject_path)["tool"]["poetry"]["version"]
-except Exception as e:
-    __version__ = "unknown"
-    print(f"Warning: Could not load version from pyproject.toml: {e}")
+__all__ = ["msa", "utils", "checks"]
+__version__ = "1.7.1"
